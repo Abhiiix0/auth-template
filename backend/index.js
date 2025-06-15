d4f2aa95d1b9b42b69433310bcae389a8aebb0b1
Abhishek Nayak <abhisheknayak@Thumbstacks-MacBook-Air.local>
2025-06-15 23:31:32 +0530

    frontend

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; // port
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
